aedad22d11e322d27a3020324cb9d1faef01f4f4
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-28 00:42:01 +0530

    completed header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/js/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
+    <div className="app">
+      <Header />
     </div>
   );
 }
