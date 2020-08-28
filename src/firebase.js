31f7b6cbdca15e3c857569046a12ed2d6c4a131a
Satya Vamsi <satya.vamsi@alphanodus.com>
2020-08-28 16:30:20 +0530

    removed firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,3 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCxwkkyIO0K2FTXVujN2Pi1xbm0jCgdDPc",
-    authDomain: "facebook-svamsi.firebaseapp.com",
-    databaseURL: "https://facebook-svamsi.firebaseio.com",
-    projectId: "facebook-svamsi",
-    storageBucket: "facebook-svamsi.appspot.com",
-    messagingSenderId: "818004928283",
-    appId: "1:818004928283:web:726a691a89fc7641f795be",
-    measurementId: "G-Y5JFHP8HKJ"
+
 };
